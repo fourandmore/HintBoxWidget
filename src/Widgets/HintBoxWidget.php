92b93cfa5cfc6e04dc5af74762793582036f6953
fourandmore <steven.resch@four-and-more.net>
2025-12-10 14:51:39 +0100

    Simplify getSettings method in HintBoxWidget
    Removed custom settings from HintBoxWidget to prevent broken structures in Vue.

--- src/Widgets/HintBoxWidget.php
+++ src/Widgets/HintBoxWidget.php
@@ -4,7 +4,6 @@ namespace HintBoxWidget\Widgets;
 
 use Ceres\Widgets\Helper\BaseWidget;
 use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
-use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
 use Ceres\Widgets\Helper\WidgetTypes;
 
 class HintBoxWidget extends BaseWidget
@@ -21,48 +20,20 @@ class HintBoxWidget extends BaseWidget
     public function getData(): array
     {
         return WidgetDataFactory::make("HintBoxWidget::HintBox")
-            ->withLabel("Widget.hintBoxLabel")                     // Text aus widgets.properties
-            ->withPreviewImageUrl("/images/hintbox-widget.svg")    // optional
-            ->withType(WidgetTypes::STATIC)                        // Content-Widget
-            ->withCategories(["text"])                             // Tab "Text" im ShopBuilder
+            ->withLabel("Widget.hintBoxLabel")
+            ->withPreviewImageUrl("/images/hintbox-widget.svg") // optional
+            ->withType(WidgetTypes::STATIC)                     // Content-Widget
+            ->withCategories(["text"])                          // erscheint im Tab "Text"
             ->withPosition(2100)
             ->toArray();
     }
 
     /**
-     * Einstellungen für das Widget im ShopBuilder
+     * KEINE eigenen Settings – leeres Array.
+     * So können hier keine kaputten Settings-Strukturen Vue zerschießen.
      */
     public function getSettings(): array
     {
-        /** @var WidgetSettingsFactory $settings */
-        $settings = pluginApp(WidgetSettingsFactory::class);
-
-        // Standard-Gruppen von Ceres
-        $settings->createCustomClass();
-        $settings->createAppearance();
-        $settings->createSpacing();
-
-        // Eigene Felder:
-        $settings->createText("headline", [
-            "name"    => "Widget.hintBoxHeadlineLabel",
-            "tooltip" => "Widget.hintBoxHeadlineTooltip"
-        ]);
-
-        $settings->createNoteEditor("text", [
-            "name"    => "Widget.hintBoxTextLabel",
-            "tooltip" => "Widget.hintBoxTextTooltip"
-        ]);
-
-        $settings->createSelect("style", [
-            "name"          => "Widget.hintBoxStyleLabel",
-            "tooltip"       => "Widget.hintBoxStyleTooltip",
-            "listBoxValues" => [
-                [ "value" => "info",    "caption" => "Widget.hintBoxStyleInfo",    "position" => 0 ],
-                [ "value" => "success", "caption" => "Widget.hintBoxStyleSuccess", "position" => 1 ],
-                [ "value" => "warning", "caption" => "Widget.hintBoxStyleWarning", "position" => 2 ]
-            ]
-        ], "info");
-
-        return $settings->toArray();
+        return [];
     }
 }
